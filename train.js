8fd2dcf8f067a96c90bdb623d4a3797e4394cfe5
Mikhail <m.kashin007@gmail.com>
2023-02-24 17:50:49 +0300

    Добавлен цикл for

--- train.js
+++ train.js
@@ -1,9 +1,6 @@
 'use strict';
 
-let number = prompt('Введите число, а я проверю четное оно или нечетное');
 
-if(number %2 == 0) {
-	console.log('Число четное');
-} else {
-	console.log('Число нечетное');
-}
+
+
+
